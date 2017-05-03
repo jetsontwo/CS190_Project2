3683ebbb170c58aaca8635bf8a8e3bef52741419
jetsontwo <jetsontwo@gmail.com>
2017-05-03 00:00:11 -0700

    Fixed Movement Script and Added Camera Follow
    Fixed Movement script and allowed player to move smoothly and also made
    the camera follow the player to give more effect.

---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+    public GameObject to_follow;
+    public float approach_speed, cutoff;
+
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 diff = new Vector3(transform.position.x - to_follow.transform.position.x, transform.position.y - to_follow.transform.position.y, 0);
+        if (Mathf.Abs(transform.position.magnitude - to_follow.transform.position.magnitude) > cutoff)
+            transform.position -= diff * approach_speed * Time.deltaTime;
+
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,7 +6,9 @@ public class Movement : MonoBehaviour {
 
     private Rigidbody2D rb;
     private float horiz;
+    private bool on_ground = false;
     public float speed, max_vel, jump_power, decel;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
@@ -15,24 +17,32 @@ public class Movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         horiz = Input.GetAxisRaw("Horizontal");
-
-        if (horiz != 0 && rb.velocity.magnitude > max_vel)
-            rb.velocity += new Vector2(horiz, 0);
+        print(horiz != 0 && rb.velocity.magnitude < max_vel);
+        if (horiz != 0 && rb.velocity.magnitude < max_vel)
+            rb.velocity += new Vector2(horiz * speed * Time.deltaTime, 0);
         else if (horiz == 0)
             rb.velocity -= new Vector2(rb.velocity.x / decel, 0);
 
         if (Input.GetKeyDown(KeyCode.Space))
-            StartCoroutine(Jump());
+            if (touching_ground())
+                rb.AddForce(new Vector2(0, jump_power), ForceMode2D.Impulse);
 
-        if(horiz != 0)
-        {
-            rb.velocity.Normalize();
-            rb.velocity *= speed * Time.deltaTime;
-        }
+        print(rb.velocity);
 	}
 
-    IEnumerator Jump()
+    public bool touching_ground()
     {
-        yield return new WaitForSeconds(0.1f);
+        return on_ground;
+    }
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        if (c.tag == "Ground")
+            on_ground = true;
+    }
+    void OnTriggerExit2D(Collider2D c)
+    {
+        if (c.tag == "Ground")
+            on_ground = false;
     }
 }
