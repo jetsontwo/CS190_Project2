46a6c271784651a81fd5ad3123ce1122be6a683a
jetsontwo <jetsontwo@gmail.com>
2017-05-07 16:57:51 -0700

    Created Explosions and Smoke for Battle Scene
    Scene now looks more like a battle with random explosions going off and
    smoke coming from the ship.

---
+++ Assets/Scripts/Explosion_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion_Controller : MonoBehaviour {
+
+    public GameObject explosion;
+	// Update is called once per frame
+	void Start()
+    {
+        StartCoroutine(explode());
+    }
+
+
+    IEnumerator explode()
+    {
+        while(true)
+        {
+            GameObject new_explosion = Instantiate(explosion);
+            new_explosion.transform.position = new Vector2(Random.Range(-30f, 30f), Random.Range(-4f, -2.5f));
+            new_explosion.GetComponent<ParticleSystem>().Play();
+            yield return new WaitForSeconds(Random.Range(0.2f, 1.5f));
+        }
+    }
+}
---
+++ Assets/Scripts/Explosion_Deleter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion_Deleter : MonoBehaviour {
+
+    ParticleSystem ps;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+	void Update () {
+        if (!ps.isEmitting)
+            Destroy(gameObject);
+	}
+}
