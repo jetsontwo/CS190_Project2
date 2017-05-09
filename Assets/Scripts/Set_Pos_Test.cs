ad9a012ab76e72d132c162a834631692742e86e7
jetsontwo <jetsontwo@gmail.com>
2017-05-09 16:50:06 -0700

    Fixed?
    Some stuff happened and it couldnt sinc so lets hope for the best.

---
+++ Assets/Scripts/Set_Pos_Test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Pos_Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AkSoundEngine.SetObjectPosition(gameObject, transform.position.x, transform.position.y, transform.position.z, 1f, 0, 0);
+    }
+}
