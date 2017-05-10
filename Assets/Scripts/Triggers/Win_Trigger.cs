ddef84bbecf2b02a2e54302048343718bc988ac2
jetsontwo <jetsontwo@gmail.com>
2017-05-10 16:22:18 -0700

    Wwise Push

---
+++ Assets/Scripts/Triggers/Win_Trigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win_Trigger : AkTriggerBase
+{
+
+    public void Win()
+    {
+        if (triggerDelegate != null)
+            triggerDelegate(null);
+    }   // Use this for initialization
+}
